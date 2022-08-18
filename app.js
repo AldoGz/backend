598b5219339a54402bd33910491f6e12aba7ddfe
Dev Aldo <soulaldo1991@gmail.com>
2022-08-18 09:28:21 -0500

    Escribir app.js

--- app.js
+++ app.js
@@ -2,25 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-function allowCrossDomain(req, res, next) {
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-
-  var origin = req.headers.origin;
-  if (_.contains(app.get("allowed_origins"), origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  if (req.method === "OPTIONS") {
-    res.send(200);
-  } else {
-    next();
-  }
-}
-
 /* Middleware */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(allowCrossDomain);
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 /* Middleware */
 
 /* ROUTERS */
